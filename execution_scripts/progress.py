e7453df8d62075906331c99f8e7ff5c0af651fdb
Fahadsule <fahadsuleiman31@gmail.com>
2026-01-30 05:05:24 +0300

    JUST ADDED BASIC GROUPING FOR PROGRESS TRACKING

--- execution_scripts/progress.py
+++ execution_scripts/progress.py
@@ -1,4 +1,14 @@
 import pandas as pd
 import sqlite3 
 
-
+conn=sqlite3.connect("data/workout.db")
+df = pd.read_sql_query("SELECT * FROM training_log", conn)
+print(df)
+bench_df=pd.read_sql_query("SELECT * FROM training_log WHERE lift='Bench'", conn)
+print(bench_df)
+current_max_df = df.groupby("lift", as_index=False)["epley_1rm"].max()
+squat_df=pd.read_sql_query("SELECT * FROM training_log WHERE lift='Squat'", conn)
+print(squat_df)
+print(current_max_df)
+deadlift_df=pd.read_sql_query("SELECT * FROM training_log WHERE lift='Deadlift'",conn)
+print(deadlift_df)
