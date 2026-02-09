401fe6ef4ec09b11e8d661ce1109e3af58b4b450
Fahadsule <fahadsuleiman31@gmail.com>
2026-02-10 01:55:40 +0300

    just fixed the categorization btn muscle and fat loss

---
+++ data_scripts/calories.py
@@ -0,0 +1,61 @@
+import pandas as pd
+from datetime import date
+
+
+#get initial dieting states
+suitable_replies=['A','B','C']
+while True:
+    input_variable = input("What phase of dieting are we in my boiii🤣\n\tchoose from the following...\n\t A FOR NONE\n\t B FOR BULK \n\t C FOR CUT   ").upper()
+    
+    if input_variable in suitable_replies:
+        diet_phase = input_variable
+        break
+    else:
+        print("Invalid response, try again!")
+
+#Get my age
+birthday = date(2006, 9, 20)  # Replace with your birthday (year, month, day)
+today = date.today()
+age = today.year - birthday.year - ((today.month, today.day) < (birthday.month, birthday.day))
+
+#the model should hypotheticallty compute the daily maintainance calories(Miflin -St Joer, Male)
+#load the data
+df=pd.read_csv("data/daily.csv")
+Height=177
+df['BMR']=(10*df['Body_weight'])+(6.25*Height)-(5*age)+5
+activity=1.6
+df['maintainance_calories']=df['BMR']*activity
+df['calorie_difference']=df['calories_taken']-df['maintainance_calories']
+
+
+df['calorie_type'] = 'maintainance'  # default value
+df.loc[df['calorie_difference'] > 90, 'calorie_type'] = 'calorie_surplus'
+df.loc[df['calorie_difference'] < 0, 'calorie_type'] = 'calorie_deficit'
+df.loc[(df['calorie_difference'] > 0) & (df['calorie_difference'] <= 90), 'calorie_type'] = 'slight_calorie_surplus'
+
+
+df['expected_mass_change']=str(0)+"kg "+"no_change"#default value
+df.loc[df['calorie_type']=='calorie_surplus', 'expected_mass_change']=(df['calorie_difference']-90)/7700
+df.loc[df['calorie_type']=='calorie_deficit', 'expected_mass_change']=df['calorie_difference']/7700
+df.loc[df['calorie_type']=='slight_calorie_surplus', 'expected_mass_change']=df['calorie_difference']/3000
+
+print(df)
+
+
+def get_maintainance():
+    today_str = today.strftime('%Y-%m-%d')  # Will be '2026-02-09'
+    today_row = df[df['l_date'] == today_str]
+    
+    if not today_row.empty:
+        return today_row['maintainance_calories'].values[0]
+    else:
+        print(f"No entry found for {today_str}")
+        return None
+    
+
+def estimate_fat_loss(start_date,end_date):
+    phase_df=df[df['l_date']>start_date]
+    phase_df=phase_df[phase_df['l_date']<end_date]
+    
+
+
--- test.py
+++ test.py
@@ -1,2 +1,5 @@
-import calories
+from data_scripts import calories
 
+today_maintainace=calories.get_maintainance()
+if today_maintainace:
+    print("Today's maintainace is, ",today_maintainace,"Kcal")
